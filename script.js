f4a0dc53568a0e2f49a3f7ddccac2a30c282dd00
Ono <tatsuya_ono_ab@mail.toyota.co.jp>
2023-02-21 17:39:04 +0900

    js修正

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// カードめくり用 タイマーID(動作中はカードがめくれないように)
 let backTimer;
-// 1枚目かどうかのフラグ(1枚目: true 2枚目: false)
 let flgFirst = true;
-// 1枚目のカードを格納
 let cardFirst;
-// そろえた枚数(ペアができるたびに+1 10ペアで終了)
 let countUnit = 0;
 
 let img_arr = [
-    "1", "2", "3", "4", "5",
-    "6", "7", "8", "9", "10"
+    "1", "2", "3", "4", "5","6"
 ];
 let img_tag_arr = [];
 for (let i = 0; i < 6; i++ ){
@@ -21,64 +16,58 @@ window.onload = function () {
     for (let i = 0; i < 6; i++) {
         arr.push(i);
         arr.push(i);
-    }  //[0,0,1,1,2,2,...........8,8,9,9] 合計20の要素
+    } 
  
-    shuffle(arr);// シャッフル [1,7,3,4,4,5......]
+    shuffle(arr);
     let game_board = document.getElementById('game_board');
 
-    // div要素作成(カード)
     for (i = 0; i < 12; i++) {
         let div = document.createElement('div');
-        div.className = 'card back'; //カードの裏側を表示
-        div.number = arr[i]; //プロパティを設定
-        //console.log(Object.keys(div));
-        //console.log(Object.values(div));
+        div.className = 'card back'; 
+        div.number = arr[i]; 
         div.onclick = turn;
         game_board.appendChild(div);
     }
-    startTime = new Date(); // 開始時刻を取得
-    startTimer(); // タイマー開始
+    startTime = new Date(); 
+    startTimer(); 
 }
-//シャッフル用関数
+
 function shuffle(arr) {
     let n = arr.length;
-    while (n) { //nが0になったら終了      ここで毎回-1
+    while (n) { 
         i = Math.floor(Math.random() * n--);
         [arr[n], arr[i]] = [arr[i], arr[n]]
     }
     return arr;
 }
-// カードクリック時の処理
+
 function turn(e) {
-    let div = e.target; //クリックしたカード
-    // カードのタイマー処理が動作中は return
-    if (backTimer) return; //連続で押せないように
-    // 裏向きのカードをクリックした場合は画像を表示する
+    let div = e.target; 
+    if (backTimer) return; 
     if (div.innerHTML == '') {
-        div.className = 'card'; //backというクラス名を取り除いた
+        div.className = 'card'; 
         div.innerHTML = img_tag_arr[div.number];
     } else {
-        return // 数字が表示されているカードは return
+        return 
     }
-    if (flgFirst) { // 1枚目の処理 一枚目ならtrue
-        cardFirst = div;  //最初にクリックしたカード
-        flgFirst = false; //次は２枚目だから
-    } else { // ２枚目の処理
+    if (flgFirst) {
+        cardFirst = div;
+        flgFirst = false;
+    } else {
         if (cardFirst.number == div.number) {
-            countUnit++; //揃ったペアの数
+            countUnit++;
             backTimer = setTimeout(function () {
-                div.className = 'card finish'; //0.5秒で透明
+                div.className = 'card finish';
                 cardFirst.className = 'card finish';
                 backTimer = NaN;
-                if (countUnit == 6) { //すべてカードが揃ったら
-                    clearInterval(timer);  // timer終了
-                    //setInterval(showSecond, 1000)
+                if (countUnit == 6) { 
+                    clearInterval(timer); 
                 }
             }, 500)
         } else {
             backTimer = setTimeout(function () {
                 div.className = 'card back';
-                div.innerHTML = ''; // カードを裏側に戻す
+                div.innerHTML = '';
                 cardFirst.className = 'card back';
                 cardFirst.innerHTML = '';
                 cardFirst = null;
