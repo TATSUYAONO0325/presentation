e8e351207f348808b7214a65381b0c1e21798de7
Ono <tatsuya_ono_ab@mail.toyota.co.jp>
2023-02-22 19:13:48 +0900

    ファイル修正3

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ window.onload = function () {
         div.onclick = turn;
         gameboard.appendChild(div);
     }
-    startTime = new Date(); 
-    startTimer(); 
+    // startTime = new Date(); 
+    // startTimer(); 
 }
 
 function shuffle(arr) {
@@ -37,7 +37,23 @@ function shuffle(arr) {
         i = Math.floor(Math.random() * n--);
         [arr[n], arr[i]] = [arr[i], arr[n]]
     }
-    console.log(arr)
+    // console.log(arr)
+    let list1 = [];
+    let list2 = [];
+    let list3 = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if (i <= 3){
+            list1.push(arr[i]);
+        } else if (i <= 7){
+            list2.push(arr[i]);
+        } else {
+            list3.push(arr[i]);
+        }
+    }
+    console.log(list1);
+    console.log(list2);
+    console.log(list3);
     return arr;
 }
 
@@ -80,8 +96,8 @@ function turn(e) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btn").addEventListener("click", function(){
-    window.location.reload();
-    })
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("btn").addEventListener("click", function(){
+//     window.location.reload();
+//     });
+// });
